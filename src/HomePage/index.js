c5115d602b50aca48d019d9d0f54a0524b30f5fe
h.benarafa <h.benarafa@gmail.com>
2023-03-15 17:50:40 +0100

    fix: 🐛 prod fix 2

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -13,7 +13,7 @@ import {
 import React from "react";
 
 export default function HomePage() {
-  const [tshirts, setTshirts] = React.useState([
+  const [tshirts] = React.useState([
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
   ]);
 
