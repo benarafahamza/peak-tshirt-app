47359898734295f719a114b11fbce6530f5c1418
h.benarafa <h.benarafa@gmail.com>
2023-03-22 14:24:59 +0000

    feat: 🎸 use object for cart page

--- src/CartPage/index.js
+++ src/CartPage/index.js
@@ -1,6 +1,7 @@
 import { Delete, ShoppingCartCheckout } from "@mui/icons-material";
 import {
   Avatar,
+  Grid,
   IconButton,
   List,
   ListItem,
@@ -24,25 +25,43 @@ TotalPrice.protoTypes = {
 };
 
 export default function CartPage() {
+  const cart = [
+    { id: 1, price: 12.34, name: "Tshirt 1", quantity: 1, imageUrl: "1.jpg" },
+    { id: 2, price: 12.34, name: "Tshirt 2", quantity: 2, imageUrl: "2.jpg" },
+  ];
+
   return (
     <>
       <List>
-        <ListItem
-          secondaryAction={
-            <IconButton edge="end" aria-label="delete">
-              <Delete />
-            </IconButton>
-          }
-        >
-          <ListItemAvatar>
-            <Avatar src="" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Single-line item"
-            secondary={"Secondary text"}
-          />
-        </ListItem>
-        ,
+        {cart.map(({ id, price, name, quantity, imageUrl }) => (
+          <ListItem
+            key={id}
+            secondaryAction={
+              <IconButton edge="end" aria-label="delete">
+                <Delete />
+              </IconButton>
+            }
+          >
+            <ListItemAvatar>
+              <Avatar src={`/images/${imageUrl}`} />
+            </ListItemAvatar>
+            <ListItemText
+              primary={
+                <Grid container>
+                  <Grid item>
+                    <Typography>{name}</Typography>
+                  </Grid>
+                  {quantity > 1 && (
+                    <Grid item sx={{ marginLeft: 1 }}>
+                      <Typography variant="caption">(x{quantity})</Typography>
+                    </Grid>
+                  )}
+                </Grid>
+              }
+              secondary={<Price value={price} />}
+            />
+          </ListItem>
+        ))}
       </List>
       <BottomCallToActions secondary={<TotalPrice price={127} />}>
         <CtaButton startIcon={<ShoppingCartCheckout />} fullWidth>
